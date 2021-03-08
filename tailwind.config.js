3a61e25ba8d4cfd3bb61a2641174dea1a9264ec6
Tom Nightingale <tom.nightingale@adtrak.co.uk>
2021-03-08 21:06:34 +0000

    strip down starter

---
+++ tailwind.config.js
@@ -0,0 +1,93 @@
+module.exports = {
+  purge: {
+      content: ['./pages/**/*.js', './components/**/*.js'],
+      options: {
+        safelist: [
+            /^wp/,
+            /^ld-/,
+            "textarea",
+            "closed",
+            "open",        
+            'sub-menu',
+            "sub-arrow",
+            "show-arrow",
+            'mob-nav--active',
+            'mob-nav-underlay',
+            'mob-nav-close',
+            "current-page-ancestor"
+        ],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      "2xs": "375px",
+      xs: "480px",
+      sm: "600px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1400px",
+      "3xl": "1600px",
+      "4xl": "1900px",
+    },
+    fontFamily: {
+      sans: ["Source Sans Pro", "sans-serif"],
+    },
+    filter: {
+      none: "none",
+      grayscale: "grayscale(1)",
+    },
+    extend: {
+      colors: {
+        primary: {
+          light: "#eee",
+          DEFAULT: "#ccc",
+          dark: "#666",
+        },
+        secondary: {
+          light: "#f83",
+          DEFAULT: "#d0dfe5",
+          dark: "#c0ced3",
+        },
+      },
+      spacing: {
+        72: "18rem",
+        84: "21rem",
+        96: "24rem",
+        128: "32rem",
+      },
+      zIndex: {
+        "-10": "-10",
+        "-20": "-20",
+      },
+      inset: (theme, { negative }) => ({
+        full: "100%",
+        "1/2": "50%",
+        ...theme("spacing"),
+        ...negative(theme("spacing")),
+      }),
+      maxWidth: (theme) => ({
+        ...theme("spacing"),
+      }),
+      minHeight: (theme) => ({
+        ...theme("spacing"),
+        25: "25vh",
+        50: "50vh",
+        75: "75vh",
+      }),
+    },
+  },
+  variants: {
+    backgroundColor: ['responsive', 'group-hover', 'hover', 'focus', 'group-focus'],
+    textColor: ['responsive', 'group-hover', 'hover', 'focus', 'group-focus'],
+    padding: ['responsive', 'group-hover', 'hover', 'focus', 'group-focus'],
+  },
+  plugins: [
+    require("tailwindcss-filters"),
+    require('@tailwindcss/forms'),
+  ],
+  corePlugins: {
+    container: false,
+  },
+}
